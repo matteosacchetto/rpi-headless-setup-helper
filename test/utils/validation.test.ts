44b1aedb6938dc3af439ebf581a4a19e85ed36e3
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2023-07-13 17:48:03 +0200

    test: add tests for async_error_to_msg

--- test/utils/validation.test.ts
+++ test/utils/validation.test.ts
@@ -1,4 +1,4 @@
-import { error_to_msg } from '@/utils/validation';
+import { async_error_to_msg, error_to_msg } from '@/utils/validation';
 import t from 'tap';
 
 t.test('error to msg (true)', async (t) => {
@@ -37,6 +37,49 @@ t.test('error to msg (false)', async (t) => {
 
 t.test('error to msg (false)', async (t) => {
   const resposne = error_to_msg(() => {
+    throw 1;
+  });
+
+  t.type(resposne, 'string');
+  t.same(resposne, 'unknown error');
+});
+
+t.test('async error to msg (true)', async (t) => {
+  t.ok(await async_error_to_msg(async () => true));
+});
+
+t.test('async error to msg (false)', async (t) => {
+  t.notOk(await async_error_to_msg(async () => false));
+});
+
+t.test('async error to msg (true)', async (t) => {
+  t.ok(await async_error_to_msg(async (flag: boolean) => flag, true));
+});
+
+t.test('async error to msg (false)', async (t) => {
+  t.notOk(await async_error_to_msg(async (flag: boolean) => flag, false));
+});
+
+t.test('async error to msg (false)', async (t) => {
+  const resposne = await async_error_to_msg(async () => {
+    throw new Error('a');
+  });
+
+  t.type(resposne, 'string');
+  t.same(resposne, 'a');
+});
+
+t.test('async error to msg (false)', async (t) => {
+  const resposne = await async_error_to_msg(async () => {
+    throw 'a';
+  });
+
+  t.type(resposne, 'string');
+  t.same(resposne, 'a');
+});
+
+t.test('async error to msg (false)', async (t) => {
+  const resposne = await async_error_to_msg(async () => {
     throw 1;
   });
 
